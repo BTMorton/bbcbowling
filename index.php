17f8b091a49868055a1f5669b737041cc4151985
Ben Morton <ben.morton91@gmail.com>
2015-02-19 07:26:02 +0000

    Improve score calculation. Update next active score navigation

--- index.php
+++ index.php
@@ -72,29 +72,32 @@
 			}
 			tr.current, tr.current td {
 				height: 15%;
-				background-color: #337AB7;
-				color: #FFFFFF;
+				background-color: #D9EDF7;
 			}
 			.table > tbody > tr > td.score {
 				padding: 0;
 			}
-			.score .score_1, .score .score_2 {
+			.score .score_1, .score .score_2, .score .score_3 {
 				width: 50%;
 				height: 50%;
 				display: inline-block;
 				box-sizing: border-box;
 				float: left;
+				border-left: 1px solid #ddd;
 			}
 			.score .score_1 {
-				border-right: 1px solid #ddd;
+				border-left-width: 0;
 			}
-			.score .score_1, .score .score_2, .score .score_tot {
+			.score .score_1, .score .score_2, .score .score_3, .score .score_tot {
 				text-align: center;
 				font-size: 25px;
 				vertical-align: middle;
 				height: 50%;
 				line-height: 100%;
 				display: table;
+			}
+			.score.col_9 .score_1, .score.col_9 .score_2, .score.col_9 .score_3 {
+				width: 33%;
 			}
 			.score span {
 				display: table-cell;
@@ -105,6 +108,9 @@
 				border-top: 1px solid #ddd;
 				clear: both;
 				width: 100%;
+			}
+			.score .active {
+				border: 3px solid #337AB7;
 			}
 			tfoot td .btn:not(#add_player) {
 				width: 46px;
@@ -139,36 +145,58 @@
 				
 				// Score button functionality
 				$(".btn:not(#add_player)").click(function() {
-					$(".current .score .active span").text($(this).text());
+					// Get the current score from the button and update the box
+					var score = $(this).text();
+					$(".current .score .active span").text(score);
+					
+					// Get the current box, row and column index
 					var active = $(".current .score .active");
 					var row = $(".current");
 					var col = active.parent("td").index() + 1;
 					
-					if (active.attr("id") != last_score.attr("id")) {
+					// If it's been selected out of order, update to set the current box
+					if (active[0] != last_score[0]) {
 						last_score.addClass('active');
 						row.removeClass('current');
 						last_score.closest('tr').addClass('current');
-					} else if (active.hasClass('score_2')) {
+					// If it's the last frame and they get a strike or spare
+					} else if (active.hasClass('score_2') && (score == "X" || score == "/") && col == 11) {
+						$(".score_3", active.parent()).addClass('active');
+					// If we're moving to the next frame
+					} else if (active.hasClass('score_2') || active.hasClass('score_3') || (score == "X" && col < 11)) {
+						// If it's a strike, blank out the second score
+						if (score == "X" && col < 11) {
+							$(".score_2", active.parent()).html("&nbsp;");
+						}
+						
+						// If it's the last player
 						if (row.is(":last-child")) {
+							// if it's not the last frame, select the next frame in the first row
 							if (col < 11) {
 								$("tbody tr:first-child td:nth-child("+(col + 1)+") .score_1").addClass('active');
 								$("tbody tr:first-child").addClass('current');
 							}
+						// Otherwise, select the next row
 						} else {
 							row.next().addClass('current');
 							$("td:nth-child("+col+") .score_1", row.next()).addClass('active');
 						}
 						
 						row.removeClass('current');
+					// Otherwise, just get the second score
 					} else {
 						$(".score_2", active.parent()).addClass('active');
 					}
 					
+					// Clear the current selection and update the 'counter'
 					active.removeClass("active");
+					last_score = $(".active");
 					
+					// Update the scores
 					calcScore();
 				});
 				
+				// When clicking a box, it should become selected
 				$(".score_1, .score_2").click(function() {
 					$(".active").removeClass('active');
 					$(".current").removeClass('current');
@@ -176,25 +204,124 @@
 					$(this).closest("tr").addClass('current');
 				});
 				
-				// Initialise bootstrap modal
+				// Bootstrap modal autofocus
 				$(".modal").on("shown.bs.modal", function() {
 					$(".modal input").focus();
 				});
 				
+				// Initialise the score box 'counter'
 				last_score = $(".active");
 			});
 			
 			// Function to update player scores
 			function calcScore() {
+				// For each player row
 				$("tbody tr").each(function() {
 					var score = 0;
 					
+					// For each frame
 					$("td.score", this).each(function() {
-						score += parseInt($(".score_1", this).text());
-						score += parseInt($(".score_2", this).text());
+						var score1 = $(".score_1", this).text();
+						
+						// If they got a strike, value is 10 + next + next
+						if (score1 == "X") {
+							score1 = 10;
+							
+							// Get next score
+							var next = nextScore($(".score_1", this));
+							
+							if (next) {
+								var tempscore = next.text();
+								
+								// If another strike or a spare, add 10, otherwise parse number
+								if (tempscore == "X" || tempscore == "/") tempscore = 10;
+								else tempscore = parseInt(tempscore);
+								
+								// Update score and get next
+								score1 += tempscore;
+								next = nextScore(next);
+								
+								if (next) {
+									// If another strike or a spare, add 10, otherwise parse number
+									if (tempscore == "X" || tempscore == "/") tempscore = 10;
+									else tempscore = parseInt(tempscore);
+									
+									score1 += tempscore;
+								}
+							}
+						} else {
+							// Otherwise, parse number
+							score1 = parseInt(score1);
+						}
+						
+						score += score1;
+						
+						var score2 = $(".score_2", this).text();
+						
+						// If they got a spare, value is 10 + next
+						if (score2 == "/") {
+							score2 = 10;
+							
+							// Get next score
+							var next = nextScore($(".score_1", this));
+							
+							if (next) {
+								var tempscore = next.text();
+								
+								// If a strike or spare, add 10, otherwise parse number
+								if (tempscore == "X" || tempscore == "/") tempscore = 10;
+								else tempscore = parseInt(tempscore);
+								
+								score2 += tempscore;
+							}
+						} else if (score2 == "" || score2 == " " || score2 == "&nbsp;" || score2 == '\xa0') {
+							// If it's empty due to a strike
+							score2 = 0;
+						} else {
+							// Otherwise, parse number
+							score2 = parseInt(score2);
+						}
+						
+						score += score2;
+						
+						// Display current score total
 						$(".score_tot span", this).text(score);
 					});
 				});
+			}
+			
+			// Function to find the next score container
+			function nextScore(current) {
+				// If it's the first score
+				if (current.hasClass('score_1')) {
+					// If it's a strike
+					if (current.text() == "X") {
+						// If it's the last frame
+						if (current.parent().hasClass('col_9')) {
+							// Return the second score
+							return $(".score_2", current.parent());
+						} else {
+							// Return the first score of the next frame
+							return $(".score_1", current.parent().next());
+						}
+					} else {
+						// Otherwise, return the second score
+						return $(".score_2", current.parent());
+					}
+				// If it's the very final score
+				} else if (current.hasClass('score_3')) {
+					return false;
+				// If it's the second score
+				} else {
+					// If it's the last frame
+					if (current.parent().hasClass('col_9')) {
+						// Return the third score
+						return $(".score_3", current.parent());
+					} else {
+						// Always return the first from the next frame
+						return $(".score_1", current.parent().next());
+					}
+				}
 			}
 			
 			// Function to add a new player row
@@ -238,9 +365,10 @@
 				<tr class="template current" id="row_0">
 					<td class="player">Player 1</td>
 					<? for ($i = 0; $i < 10; $i++) { ?>
-					<td class="score" id="score_0_<?=$i?>">
+					<td class="score col_<?=$i?>">
 						<div class="score_1<?=$i == 0 ? ' active' : ''?>"><span>0</span></div>
 						<div class="score_2"><span>0</span></div>
+						<?=$i == 9 ? '<div class="score_3"><span>0</span></div>' : '' ?>
 						<div class="score_tot"><span>0</span></div>
 					</td>
 					<? } ?>
@@ -248,9 +376,10 @@
 				<tr class="" id="row_1">
 					<td class="player">Player 2</td>
 					<? for ($i = 0; $i < 10; $i++) { ?>
-					<td class="score" id="score_1_<?=$i?>">
+					<td class="score col_<?=$i?>">
 						<div class="score_1"><span>0</span></div>
 						<div class="score_2"><span>0</span></div>
+						<?=$i == 9 ? '<div class="score_3"><span>0</span></div>' : '' ?>
 						<div class="score_tot"><span>0</span></div>
 					</td>
 					<? } ?>
